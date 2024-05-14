206af9fed1a55f9e38da3c8c6a724e2c78f31cf8
mrodavell <jomar.llevado@ustp.edu.ph>
2024-05-14 14:23:44 +0800

    with StyledComponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import style from './stylejs/AppStyle';
+import './css/App.css';
+import { ButtonBlue, ButtonRed } from './StyledComponents/StyledComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='backgroundColorGray'>
+        <div style={style.main}>
+          <div style={style.innerDiv}>
+            <h1 style={style.header} id='fontColorRed'>Login</h1>
+            <form action="#" style={{ padding: 10 }}>
+              <div style={style.formDiv}>
+                <input type="text" placeholder="Email" style={style.input} />
+              </div>
+              <div style={style.formDiv}>
+                <input type="password" placeholder="Password" style={style.input} />
+              </div>
+              <div style={style.formDiv}>
+                <ButtonBlue>Login</ButtonBlue>
+              </div>
+              <div style={style.formDiv}>
+                <ButtonRed>Signup</ButtonRed>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/StyledComponents/StyledComponent.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const ButtonRed = styled.button`
+  background-color: red;
+  width: 100%;
+  height: 35px;
+  fontsize: 16px;
+`;
+
+const ButtonBlue = styled.button`
+  background-color: blue;
+  width: 100%;
+  height: 35px;
+  fontsize: 16px;
+`;
+
+export { ButtonRed, ButtonBlue };
